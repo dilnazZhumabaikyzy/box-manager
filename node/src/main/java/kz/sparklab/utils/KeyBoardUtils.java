4d7e3497cc3f75e2361f5a7a7856bd64cb2a2974
Yeboka <89529214+Yeboka@users.noreply.github.com>
2024-03-11 16:12:23 +0500

    copy code from main branch to KeyBoardUtils

--- node/src/main/java/kz/sparklab/utils/KeyBoardUtils.java
+++ node/src/main/java/kz/sparklab/utils/KeyBoardUtils.java
@@ -1,4 +1,30 @@
 package kz.sparklab.utils;
 
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@Component
 public class KeyBoardUtils {
+
+    public InlineKeyboardMarkup getInlineKeyboardMarkup(String... buttons) {
+
+        List<List<InlineKeyboardButton>> rowList = new ArrayList<>();
+
+        Arrays.stream(buttons).forEach((String buttonText) -> {
+            List<InlineKeyboardButton> row = new ArrayList<>();
+            row.add(InlineKeyboardButton.builder().text(buttonText).callbackData(buttonText).build());
+
+            rowList.add(row);
+        });
+
+        InlineKeyboardMarkup inlineKeyboardMarkup = new InlineKeyboardMarkup();
+        inlineKeyboardMarkup.setKeyboard(rowList);
+
+        return inlineKeyboardMarkup;
+    }
 }
